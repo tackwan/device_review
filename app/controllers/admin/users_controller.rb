cd9dcaae47364046a77bd6353d8ad57804a60082
tackwan <129726011+tackwan@users.noreply.github.com>
2023-06-13 20:09:32 +0000

    [update]admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,31 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_admin!, only: [:index,:show,:edit,:update]
+  def index
+    @users = User.page(params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      flash[:notice] = "更新に成功しました"
+      redirect_to admin_user_path(customer.id)
+    else
+      flash[:notice] = "更新に失敗しました"
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
