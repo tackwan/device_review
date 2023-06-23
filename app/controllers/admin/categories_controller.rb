a1fb79beae5d63ec0f53d5ce66c23996d7db3a98
tackwan <129726011+tackwan@users.noreply.github.com>
2023-06-23 13:25:35 +0000

    update

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,33 @@
 class Admin::CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.save
+    redirect_to admin_categories_path
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to admin_categories_path
+      flash[:success] = "編集に成功しました"
+    else
+      flash[:danger] = "編集に失敗しました"
+      render :edit
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
