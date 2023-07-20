c129cc3204c56abcbe7d32bab3e12402b18ba226
tackwan <129726011+tackwan@users.noreply.github.com>
2023-07-20 14:34:59 +0000

    レイアウトの調整

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,10 +5,6 @@ class Review < ApplicationRecord
 
   has_one_attached :image
 
-  def get_image(width, height)
-  image.variant(resize_to_limit: [width, height]).processed
-  end
-
   validates :image, presence: true
   validates :name, presence: true
   validates :detail, presence: true
